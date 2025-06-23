7f2defae18e216d89abb1321bc12df0d6b952a84
rajAutomtaion <raj116629@gmail.com>
2025-06-24 01:11:47 +0530

    adding selenium grid code

--- src/test/java/swaglab_test/BaseClass.java
+++ src/test/java/swaglab_test/BaseClass.java
@@ -1,42 +1,60 @@
 package swaglab_test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
-	
-	
+
 	public static WebDriver driver;
-	
+
 	@BeforeMethod
-	public void setUp()
-	{
-		
-		String BrowserName =System.getProperty("Browser");
-		
-		if(BrowserName.equalsIgnoreCase("firefox"))
-		{
-			driver= new FirefoxDriver();
+	public void setUp() throws MalformedURLException {
+
+		String BrowserName = System.getProperty("Browser");
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+		if (BrowserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
 		}
-		
+
+		else if (BrowserName.equalsIgnoreCase("firefox_grid")) {
+
+			cap.setBrowserName("firefox");
+			cap.setPlatform(Platform.WIN11);
+			URL Huburl = new URL("http://localhost:4444");
+			driver = new RemoteWebDriver(Huburl, cap);
+
+		}
+
+		else if (BrowserName.equalsIgnoreCase("chrome_grid")) {
+			cap.setBrowserName("chrome");
+			cap.setPlatform(Platform.WIN11);
+			URL Huburl = new URL("http://localhost:4444");
+			driver = new RemoteWebDriver(Huburl, cap);
+		}
+
 		else {
-			driver= new ChromeDriver();
-			
+			driver = new ChromeDriver();
+
 		}
-		driver= new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
-	
+
 	@AfterMethod
-	public void tearDown()
-	{
+	public void tearDown() {
 		driver.quit();
 	}
 
